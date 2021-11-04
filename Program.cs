ed0e92dbcc3ce20c3fb846670ab257f49f5b9cb6
Andrew Kara <67162220+tatakaeq@users.noreply.github.com>
2021-11-04 18:47:17 +0200

    DONE

--- Program.cs
+++ Program.cs
@@ -33,9 +33,55 @@ namespace Dz4
             var arr3 = new int[oddCounter];
             arr2 = arr1.Where(x => x % 2 == 0).ToArray();
             arr3 = arr1.Where(x => x % 2 != 0).ToArray();
+            var resArr1 = new char[arr2.Length];
+            var resArr2 = new char[arr3.Length];
+            for (var i = 0; i < arr2.Length; i++)
+            {
+                var resultEven = (char)(arr2[i] + 96);
+                resArr1[i] = resultEven;
+            }
+
             for (var i = 0; i < arr3.Length; i++)
             {
-                Console.WriteLine(arr3[i]);
+                var resultOdd = (char)(arr3[i] + 96);
+                resArr2[i] = resultOdd;
+            }
+
+            for (var i = 0; i < resArr1.Length; i++)
+            {
+                if (resArr1[i] == 'a' ||
+                    resArr1[i] == 'e' ||
+                    resArr1[i] == 'i' ||
+                    resArr1[i] == 'd' ||
+                    resArr1[i] == 'h' ||
+                    resArr1[i] == 'j')
+                {
+                    resArr1[i] = (char)((int)resArr1[i] - 32);
+                }
+            }
+
+            for (var i = 0; i < resArr2.Length; i++)
+            {
+                if (resArr2[i] == 'a' ||
+                    resArr2[i] == 'e' ||
+                    resArr2[i] == 'i' ||
+                    resArr2[i] == 'd' ||
+                    resArr2[i] == 'h' ||
+                    resArr2[i] == 'j')
+                {
+                    resArr2[i] = (char)((int)resArr2[i] - 32);
+                }
+            }
+
+            foreach (var c in resArr1)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+            foreach (var c in resArr2)
+            {
+                Console.Write(c + " ");
             }
         }
     }
